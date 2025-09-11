3e84fd2eff28d3c4cc090db728a09693c24cea6d
testing525 <ketchupash273@gmail.com>
2025-09-11 22:09:45 +0000

    fix unused and duplicated scripts

--- Assets/Scripts/TownScripts/NPCDialogueSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "NPC/Dialogue", fileName = "NewDialogue")]
-public class NPCDialogueSO : ScriptableObject
-{
-    [TextArea(3, 6)]
-    public string[] dialogueLines;
-}
